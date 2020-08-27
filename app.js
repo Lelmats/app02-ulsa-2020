df2721a98544d47c249eff0fa60fbd0be0e0d66c
Lelmats <maxalejandrorh@hotmail.com>
2020-08-26 23:58:37 -0700

    Intento de pokedex

--- app.js
+++ app.js
@@ -1,20 +1,75 @@
 const pokemon = 
 {
     sprite: document.getElementById('pokemon-image'),
-    name: document.getElementById('pokemon-name')
+    name: document.getElementById('pokemon-name'),
+    types: document.getElementById('pokemon-types'),
+    abilities: document.getElementById('pokemon-abilities'),
 }
 
-const GetPokemon  = async () =>
+/*const GetPokemon  = async () =>
 {
     const url = 'https://pokeapi.co/api/v2/pokemon/porygon';
-
     const data = await fetch(url);
     const dataJson = await data.json();
-
-    console.log(await dataJson);
- 
-    pokemon.sprite.src = await dataJson.sprites.front_default;
-    pokemon.name.innerHTML = await `Name: ${dataJson.name}`;
+    const {sprites, name, types, abilities} = dataJson;
+    types.forEach(element => 
+    {
+        const {type} = element;
+        //console.log(element.type.name);
+        pokemon.types.innerHTML += `<li>${type.name}</li>`;
+    });
+    abilities.forEach(element => 
+    {
+        const {ability} = element;
+        // pokemon.abilities.innerHTML += `<li>${ability.name}</li>`;
+        GetAbilityInfo(ability.url,  ability.name);
+    });
+    pokemon.sprite.src = await sprites.front_default;
+    pokemon.name.innerHTML = await `Name: ${name}`;
 }
+const GetAbilityInfo= async (url, abilityName)=>{
+    const data = await fetch(url);
+    const dataJson = await data.json();
+    const{effect_entries} = dataJson;
+    let effectsList = '';
+    effect_entries.forEach(element => {
+        const{effect, language} = element;
+            if(language.name === 'en')
+            {
+                 //console.log(effect);
+                effectsList += `<li>${effect}</li>`;
+            }
+    });
+    pokemon.abilities.innerHTML += 
+    `<li>
+        ${abilityName}
+        <div>Effect</div>
+        <ul>
+            ${effectsList}
+        </ul>
+    </li>`;
+}*/
+const GetPokemonlist= async() =>
+{
+    
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
 
-GetPokemon();
+    fetch(url).then(response =>
+    {
+        return response.json();
+    }).then(data =>
+        {
+            const html = data.results.map(poke => {
+                    return `
+                    <div id=pokemons>
+               
+                    <p>Name: ${poke.name}</p>
+                    </div>
+                    `   
+                }).join('')
+                console.log(html)
+            console.log(data)
+            document.querySelector("#pokemon-name").insertAdjacentHTML("beforebegin", html)
+        })
+}
+GetPokemonlist();
